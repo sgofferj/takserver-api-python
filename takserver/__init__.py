62cce5bdacdae993caea154c28d04c4b748a4db8
sgofferj <stefan@gofferje.net>
2025-07-17 17:03:58 +0300

    Redoing directory structure

---
+++ takserver/__init__.py
@@ -0,0 +1,27 @@
+"""This is the '__init__.py' of the 'takserver' package!"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# takserver.py from https://github.com/sgofferj/takserver-api-python
+#
+# Copyright Stefan Gofferje
+#
+# Licensed under the Gnu General Public License Version 3 or higher (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#from __future__ import annotations
+
+from ._tak_class import takserver
+#from ._tak_function import takFunction
+
+__all__ = [
+    "takserver"#,
+#    "takFunction",
+]
